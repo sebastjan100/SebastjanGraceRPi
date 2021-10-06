357d27395ac614f1cfd23fe1c7b2ecd5166ad965
Sebastjan Tekavc <sebastjan.tekavc@gmail.com>
2021-10-06 16:45:23 +0200

    add print at the end.

--- vlaga.py
+++ vlaga.py
@@ -2,11 +2,16 @@ import Adafruit_DHT
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-
+print(" ˙C    I    vlaga%")
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None and temperature is not None:
-        print("temperatura je", temperature, "vlaga je", humidity)
+        if humidity is 90:
+            print("vlaga je zelo visoka")
+        else:
+            print("")
+        print(int(temperature), "    I    ", int(humidity))
     else:
         print("ni bilo mogoče uspostaviti povezave s senzorjem, preverite povezavo")
+	print("to je sprememba")
