91d363bb6dc891e8eb39e6ce7025270e260c74a2
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2021-10-20 13:50:10 +0000

    some redesaining code

--- led.py
+++ led.py
@@ -5,25 +5,7 @@ GPIO.setwarnings(False)
 GPIO.setup(22,GPIO.OUT)
 print "LED on"
 GPIO.output(22,GPIO.HIGH)
-time.sleep(0.5)
-print "LED off"
-GPIO.output(22,GPIO.LOW)
-time.sleep(0.5)
-print "LED on"
-GPIO.output(22,GPIO.HIGH)
-time.sleep(0.5)
-print "LED off"
-GPIO.output(22,GPIO.LOW)
-time.sleep(0.5)
-print "LED on"
-GPIO.output(22,GPIO.HIGH)
-time.sleep(0.5)
-print "LED off"
-GPIO.output(22,GPIO.LOW)
 time.sleep(1.5)
-print "LED on"
-GPIO.output(22,GPIO.HIGH)
-time.sleep(0.5)
 print "LED off"
 GPIO.output(22,GPIO.LOW)
 time.sleep(0.5)
@@ -38,10 +20,24 @@ GPIO.output(22,GPIO.HIGH)
 time.sleep(0.5)
 print "LED off"
 GPIO.output(22,GPIO.LOW)
+time.sleep(0.5)
+print "LED on"
+GPIO.output(22,GPIO.HIGH)
 time.sleep(1.5)
+print "LED off"
+GPIO.output(22,GPIO.LOW)
+time.sleep(0.5)
 print "LED on"
 GPIO.output(22,GPIO.HIGH)
 time.sleep(0.5)
 print "LED off"
 GPIO.output(22,GPIO.LOW)
+time.sleep(0.5)
+print "LED on"
+GPIO.output(22,GPIO.HIGH)
+time.sleep(0.5)
+print "LED off"
+GPIO.output(22,GPIO.LOW)
+print "LED end"
+
 
