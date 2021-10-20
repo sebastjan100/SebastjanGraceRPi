543a7f61b4b25e108a7ed74e995807fd69f77ecd
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2021-10-20 15:48:21 +0000

    zamenu sem kodo od ozbeja

--- jakost.py
+++ jakost.py
@@ -1,77 +1,56 @@
-from RPi import GPIO
+#pwm = pulse width modulation
+import RPi.GPIO as GPIO   # Import the GPIO library.
+import time               # Import time library
 
-rdeca = 22
+GPIO.setmode(GPIO.BCM)  # Set Pi to use pin number when referencing GPIO pins.
+# Can use GPIO.setmode(GPIO.BCM) instead to use 
+# Broadcom SOC channel names.
 
-step = 5
-paused = False 
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(rdeca, GPIO.OUT)
-pwm = GPIO.PWM(rdeca, 100)
-
+led = 22
 clk = 17
 dt = 18
 sw = 27
-counter = 0
-
-pwm.start(counter)
+dc=5# set dc variable to 0 for 0%
 
 GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-
-
-clkLastState = GPIO.input(clk)
-dtLastState = GPIO.input(dt)
-swLastState = GPIO.input(sw)
+GPIO.setup(led, GPIO.OUT)  # Set GPIO pin 12 to output mode.
+pwm = GPIO.PWM(led, 100)   # Initialize PWM on pwmPin 100Hz frequency
 
 def clkClicked(channel):
-        global counter
-        global step
-
+        global dc
+ 
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
-        if clkState == 0 and dtState == 1:
-                counter = counter + step
-                pwm.ChangeDutyCycle(counter)
-                print ("Counter ", counter)
 
+        if clkState == 0 and dtState == 1 and dc < 96:#if dt's state is 1 and clk's state is 0 that means that dt is infront of clk and so we are moving clockwise
+                dc += 5
+ 
 def dtClicked(channel):
-        global counter
-        global step
-
+        global dc
+        #Premikamo se v smeri urinega kazalca, če je pin 17 == 1 in pin 18 == 0
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
-        
-        if clkState == 1 and dtState == 0:
-                counter = counter - step
-                pwm.ChangeDutyCycle(counter)
-                print ("Counter ", counter)
 
+        if clkState == 1 and dtState == 0 and dc > 4:
+                dc -= 5
 
-def swClicked(channel):
-        global paused
-        paused = not paused
+GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=20)
+GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=20)
 
+# main loop of program
+print("\nPress Ctl C to quit \n")  # Print blank line before and after message.
+pwm.start(dc)                      # Start PWM with 0% duty cycle
 
+try:
+    while True:                      # Loop until Ctl C is pressed to stop.
+        pwm.ChangeDutyCycle(dc)
+        time.sleep(0.05)             # wait .05 seconds at current LED brightness
+        print(dc)
+except KeyboardInterrupt:
+    print("Ctl C pressed - ending program")
 
-GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=50)
-GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=50)
-GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=50)
-
-input("Start monitoring input")
-
-
-if counter >= 100:
-        counter -= 5
-        print("deluje 1")
-
-if counter <= 0:
-        counter += 5
-        print("deluje 2")
-
-print("\nCtl C pressed - ending program")
-
-pwm.stop()
-GPIO.cleanup()
+pwm.stop()                         # stop PWM
+GPIO.cleanup()                     # resets GPIO ports used back to input mode
