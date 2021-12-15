0903a9b3d38c878011cc901096081eb8359be420
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2021-12-15 16:26:09 +0000

    update vlaga.py

--- vlaga.py
+++ vlaga.py
@@ -1,17 +1,15 @@
 import Adafruit_DHT
+import uzSenzor
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
-print(" ˙C    I    vlaga%")
+
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
+        razd = uzSenzor.distance()
+        print("Razdalja, ki jo izberemo, znasa%.lf cm. " % razd)
     if humidity is not None and temperature is not None:
-        if humidity is 90:
-            print("vlaga je zelo visoka")
-        else:
-            print("")
-        print(int(temperature), "    I    ", int(humidity))
+        print("Temperatura je", temperature, "vlaga pa znaša", humidity)
     else:
         print("ni bilo mogoče uspostaviti povezave s senzorjem, preverite povezavo")
         print("to ni bilo mogoce")
