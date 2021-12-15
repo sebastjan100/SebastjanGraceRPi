e27b81049001e0a4a25e3383824d0d9556adb29e
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2021-12-15 16:09:42 +0000

    uzSenzor read.

--- uzSenzor.py
+++ uzSenzor.py
@@ -3,3 +3,36 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
+trig = 26
+echo = 24
+
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+
+def distance():
+
+        GPIO.output(trig, 1)#zapiskamo
+        time.sleep(0.00001)#pocakamo  0.01ms
+        GPIO.output(trig, 0)#nehamo piskat
+
+        zacetniCas = time.time()
+        koncniCas = time.time()
+
+        while GPIO.input(echo) == 0:#cakamo dokler je tisina
+                zacetniCas = time.time()#zapisemo si zadnji cas ko je tisina
+
+        while GPIO.input(echo) == 1:#cakamo dokler slisimo pisk
+                koncniCas = time.time()#zapisemo si cas konca piska
+
+        razlikaCas = koncniCas - zacetniCas
+        
+        razdalja = (razlikaCas * 34200)/2#izracujnamo razdaljo, delimo z dve ker zvok prepotuje dvojno pot do ovire
+
+        print("v spremenjivki __name__ se skriva", __name__)
+        return razdalja
+
+if __name__ == "__main__":# izvajamo skripto uzSenzor
+        while True:
+                razd = distance()
+                print("izmerjena razdalja je", razd, "cm.")
+                time.sleep(2)
