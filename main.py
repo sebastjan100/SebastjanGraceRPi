c63b0d0ababa62b40ba0dfe96d67eddca05b96a0
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-02-22 10:12:41 +0000

    bug fixes

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ while True:
 
     # Write two lines of text.
     draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-    draw.text((x, top + 8),     "razd: "str(razd), font=font, fill=255)
-    draw.text((x, top + 16),    "hum: "str(humidity),  font=font, fill=255)
-    draw.text((x, top + 25),    "temp: "str(temperature),  font=font, fill=255)
+    draw.text((x, top + 8),     "razd: " + str(razd), font=font, fill=255)
+    draw.text((x, top + 16),    "hum: " + str(humidity),  font=font, fill=255)
+    draw.text((x, top + 25),    "temp: " + str(temperature),  font=font, fill=255)
 
 try: 
     UZ.inicialize(26, 19)
