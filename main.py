4bbdf6709abdc8da5f0e857737b3fe18f3286048
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-02-22 09:32:28 +0000

    enako kot na rpi

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("this file is empty")
+import RPi.GPIO as GPIO
+import time
+import uzSenzor as UZ
+
+trig = 26
+echo = 19
+
+try:
+    UZ.inicialize(trig, echo)
+    while True:
+        razd = UZ.distance(trig, echo)
+        print("Izmerjena razdalja je", razd, "cm.")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("Uporabnik je pritisnil ctrl + c.")
+    GPIO.cleanup()
