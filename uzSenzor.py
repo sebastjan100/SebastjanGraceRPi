4d3048ae42fcb603fded6f8e593f42b32f2d6ec8
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-23 14:57:05 +0000

    nazaj

--- uzSenzor.py
+++ uzSenzor.py
@@ -39,7 +39,7 @@ if __name__ == "__main__": #izvajamo skripto uzSenzor
                 initialize(trig, echo)
                 while True:
                         razd = distance(trig, echo)
-                        print("Izmerjena razdalja je", float(razd, 3), "cm.")
+                        print("Izmerjena razdalja je", razd, "cm.")
                         time.sleep(2)
         except KeyboardInterrupt:
                 print("Uporabnik je pritisnil ctrl + c.")
