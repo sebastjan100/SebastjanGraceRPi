f75ac4ac9c2853f8201fb0c03764e08a50651903
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-28 14:06:14 +0000

    main2.py

--- poslji.py
+++ poslji.py
@@ -3,11 +3,28 @@ def posljiUZ(razd, url, endpoint, apikey):
     headers = {
         "Authorization" : "Bearer " + apikey
     }
-    dat = {
+    data = {
         "distance": razd
     }
-    response = requests.post(url+endpoint, json=dat, headers=headers)
+    response = requests.post(url+endpoint, json=data, headers=headers)
     print(response.status_code)#ce je 200 je ok, ce pa kar kol druzga pa ne ok
 
-def posljiDHT():
-    print("poslano DHT")
+def posljiDHT(dhtData, url, endpoint, apikey):
+    headers = {
+        "Authorization" : "Bearer " + apikey
+    }
+    data = {
+        "temp": dhtData[1]
+        "hum": dhtData[0]
+    }
+    response = requests.post(url+endpoint, json=data, headers=headers)
+    print(response.status_code)#ce je 200 je ok, ce pa kar kol druzga pa ne ok
+
+def posljiPIR():
+    headers = {
+        "Authorization" : "Bearer " + apikey
+    }
+    data = {
+        "temp": dhtData[1]
+        "hum": dhtData[0]
+    }
