6752e24286bb73f7ba975e5e1115785ccf191c39
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-29 08:11:29 +0000

    pascal's code

--- main.py
+++ main.py
@@ -1,48 +1,25 @@
 import RPi.GPIO as GPIO
-import Adafruit_DHT
 import time
-#####dht
-import vlaga as DHT
 
-#####uz
-import uzSenzor as US
-
-#####poslji
+import uzSenzor as UZ
+import dht as DHT
 import poslji
 
-#####Web app tingi
-url = "https://GraceWebApp-SebastjanTekavc.online404.repl.co"
-apikey = "sebastjan"
-
-
-#####oled ekran
-#import Adafruit_GPIO.SPI as SPI
 import Adafruit_SSD1306
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 import subprocess
 
-#import uzSenzor as UZ
-
+# Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
 
+# 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-#####dht
-DHT_SENSOR = Adafruit_DHT.DHT11
-
-#####uz senzor
-trig = 26
-print(trig)
-echo = 19
-print(echo)
+# web app data
+url = "kva te briga moj url?"
+apikey = "kva te briga moj key?"
 
 # Initialize library.
 disp.begin()
@@ -51,13 +28,16 @@ disp.begin()
 disp.clear()
 disp.display()
 
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
-
 image = Image.new('1', (width, height))
 
 # Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 # Draw some shapes.
@@ -68,42 +48,42 @@ bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
+
 # Load default font.
 font = ImageFont.load_default()
 
-try:
-    trig = 26
-    echo = 19
-    US.initialize(trig, echo)
-    
-    dht_pin = 4
-    DHT.setDHT_pin(dht_pin)
-    while True:
-        # Draw a black filled box to clear the image.
-        draw.rectangle((0,0,width,height), outline=0, fill=0)
-    
-        cmd = "hostname -I | cut -d\' \' -f1"
-    
-        IP = subprocess.check_output(cmd, shell = True )
-        razd = US.distance(trig, echo)
-        humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, dht_pin)
-        poslji.posljiUZ(razd, url, "/api/uz/", apikey)
-    
-        # Write two lines of text.
-        draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
-        draw.text((x, top + 8),     "razd: " + str(razd), font=font, fill=255)
-        draw.text((x, top + 16),    "hum: " + str(humidity),  font=font, fill=255)
-        draw.text((x, top + 25),    "temp: " + str(temperature),  font=font, fill=255)
-        poslji.posljiDHT(data , url, "/api/dht/", apikey)
-        poslji.posljiPIR(pir , url, "/api/pir/", apikey)
-        
-        # Display image.
-        disp.image(image)
-        disp.display()
-        time.sleep(2)
 
+trig = 26
+echo = 19
+
+
+UZ.initialize(trig, echo) 
+
+dht_pin = 4
+DHT.setDHT_pin(dht_pin)
+
+try: 
+        while True:
+                draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+                cmd = "hostname -I | cut -d\' \' -f1"
+                IP = subprocess.check_output(cmd, shell = True )
+                draw.text((x, top),       "IP: " + str(IP),  font=font, fill=255)
+
+                razd = UZ.distance(trig, echo)
+                print("Izmerjena razdalja je", razd, "cm.")
+                draw.text((x, top+8),  "Razdalja:" + str(round(razd,2)) + " cm", font=font, fill=255)
+                poslji.posljiUZ(razd, url, "/api/uz/", apikey)
+
+                data = DHT.get_humidity()
+                print("Vlaga", data[0], "temp", data[1])
+                draw.text((x, top+8),  "HUM:" + str(data[0]) + " %", font=font, fill=255)
+                draw.text((x, top+8),  "TEMP:" + str(data[1]) + " cm", font=font, fill=255)
+                poslji.posljiDHT(data, url, "/api/dht/", apikey)
+
+                disp.image(image)
+                disp.display()
+                time.sleep(1)
 except KeyboardInterrupt:
-    print("Uporabnik je pritisnil ctrl + c.")
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
-    disp.display()
-    GPIO.cleanup()
+        print("Uporabnik je pritisnil ctrl + c.")
+        GPIO.cleanup()
