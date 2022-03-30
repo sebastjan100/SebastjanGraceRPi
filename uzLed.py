ec7f6e7b89e155d9a7142871133a47951e6ea443
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-30 12:53:21 +0000

    try it again

--- uzLed.py
+++ uzLed.py
@@ -81,18 +81,28 @@ if __name__ == "__main__": #izvajamo skripto uzSenzor
                             b = random.randint(0,255)
                             if r > 0 and g > 0:
                                 b = 0
+                                r = random.randint(0,255)
+                                g = random.randint(0,255)
                             pixels[0] = (r,g,b)
                             if g > 0 and b > 0:
                                 r = 0
+                                g = random.randint(0,255)
+                                b = random.randint(0,255)
                             pixels[1] = (r,g,b)
                             if b > 0 and r > 0:
                                 g = 0
+                                r = random.randint(0,255)
+                                b = random.randint(0,255)
                             pixels[2] = (r,g,b)
                             if r > 0 and g > 0:
                                 b = 0
+                                r = random.randint(0,255)
+                                g = random.randint(0,255)
                             pixels[3] = (r,g,b)
                             if g > 0 and b > 0:
                                 r = 0
+                                g = random.randint(0,255)
+                                b = random.randint(0,255)
                             pixels[4] = (r,g,b)
                     
                     
