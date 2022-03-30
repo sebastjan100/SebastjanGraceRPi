b6f79935c5c43e990b04061acc7fe03ca62fc53f
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-30 12:23:28 +0000

    reposition

--- uzLed.py
+++ uzLed.py
@@ -44,6 +44,32 @@ def distance(trig, echo):
         #print("V spremenljivki __name__ se skriva", __name__)
         return razdalja
 
+
+
+
+
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos * 3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos * 3)
+        g = 0
+        b = int(pos * 3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos * 3)
+        b = int(255 - pos * 3)
+    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
+
 def rainbow_cycle(wait):
     for j in range(255):
         for i in range(num_pixels):
@@ -51,6 +77,8 @@ def rainbow_cycle(wait):
             pixels[i] = wheel(pixel_index & 255)
         pixels.show()
         time.sleep(wait)
+
+
 
 if __name__ == "__main__": #izvajamo skripto uzSenzor
         try:
@@ -101,24 +129,5 @@ if __name__ == "__main__": #izvajamo skripto uzSenzor
 
 
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
+
 
