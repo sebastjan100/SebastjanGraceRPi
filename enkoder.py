20b5bba212f7d4cf0dd5089e667443e215cde206
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-30 15:24:59 +0000

    petra

--- enkoder.py
+++ enkoder.py
@@ -1,33 +1,34 @@
 #uvozimo knjiznico za delo z GPIO pini
 from RPi import GPIO
 
+#####GLOBALNO!!!!!!!!!!!!!!
+counter = 0#stevec zacne steti s 0
+
 
 step = 5 #shranimo korak, ki ga pristejemo ali odstejejo, ko se zavrti enkoder
 paused = False #spremljamo stanje, kdaj se ustavimo
 
-#povemo kako bomo nastavili GPIO pine; uporabljamo logicni nacin, to kar je napisano na GPIO clenu
-GPIO.setmode(GPIO.BCM)
+def initialize(clk,dt,sw):
 
-#clk in st sta spremenjlivki, ki povesta kam smo povezali pina (s1, s2)
-#sw je spremenjivka, ki pove kam je povezana tipka(key)
-clk = 17
-dt = 5
-sw = 27
+    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    
+    #preverimo in nastavimo prvotno stanje
+    
+    clkLastState = GPIO.input(clk)#preberemo stanje na pinu 17
+    dtLastState = GPIO.input(dt)#preberemo stanje na pinu 18
+    swLastState = GPIO.input(sw)#preberemo stanje na pinu 27 - key
 
-#nastavimo vhodne in izhodne enote
-#GPIO.setup(pin, vhod/izhod, nacin)
-GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    print ("Initial clk:", clkLastState)
+    print ("Initial dt:", dtLastState)
+    print ("Initial sw:", swLastState)
+    print ("=========================================")
 
-#preverimo in nastavimo prvotno stanje
-counter = 0#stevec zacne steti s 0
-clkLastState = GPIO.input(clk)#preberemo stanje na pinu 17
-dtLastState = GPIO.input(dt)#preberemo stanje na pinu 18
-swLastState = GPIO.input(sw)#preberemo stanje na pinu 27 - key
 
-#define functions which will be triggered on pin state changes
-def clkClicked(channel):
+def clkKlik(clk,dt):
+    
+    def clkClicked(channel):
         global counter#uporabimo globalni spremenjivko counter
         global step#uporabimo globalni spremenjivko step
 
@@ -39,8 +40,10 @@ def clkClicked(channel):
                 #pristejemo korak - vrtimo se v smeri urinega kazalca
                 counter = counter + step
                 print ("Counter ", counter)
+    return clkClicked
 
-def dtClicked(channel):
+def dtKlik(clk,dt):
+    def dtClicked(channel):
         global counter
         global step
 
@@ -52,6 +55,7 @@ def dtClicked(channel):
                 #odstejemo korak - vrtimo se v obratni smeri urinega kazalca
                 counter = counter - step
                 print("Counter ", counter)
+    return dtClicked
 
 
 def swClicked(channel):
@@ -59,17 +63,26 @@ def swClicked(channel):
         paused = not paused#obrnemo vrednost spremenjivke poused
         print ("Paused ", paused)
 
-#na zacetku sprintamo zacetno staje
-print ("Initial clk:", clkLastState)
-print ("Initial dt:", dtLastState)
-print ("Initial sw:", swLastState)
-print ("=========================================")
+defCounter():
+    global counter
+    return counter
 
-#nastavimo prekinitve
-#GPIO.add_event_detect(pin, kdaj preberemo FALLING/RISING/ONCHANGE, katero funkcijo naj izvede, nastavimo koliko casa naj prekinitev pocaka pred ponovno izvedbo)
-GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=50)
-GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=50)
-GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=50)
+def getClick():
+    global paused
+    return paused
+
+if __name__ =="__main__":
+    clk = 17
+    dt = 5
+    sw = 27
+
+    initialize(clk, dt, sw)
+    
+    #nastavimo prekinitve
+    #GPIO.add_event_detect(pin, kdaj preberemo FALLING/RISING/ONCHANGE, katero funkcijo naj izvede, nastavimo koliko casa naj prekinitev pocaka pred ponovno izvedbo)
+    GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkKlik(clk,dt), bouncetime=50)
+    GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked(clk,dt), bouncetime=50)
+    GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=50)
 
 input("Start monitoring input")
 
---
+++ test_enkoder.py
@@ -0,0 +1,77 @@
+#uvozimo knjiznico za delo z GPIO pini
+from RPi import GPIO
+
+
+step = 5 #shranimo korak, ki ga pristejemo ali odstejejo, ko se zavrti enkoder
+paused = False #spremljamo stanje, kdaj se ustavimo
+
+#povemo kako bomo nastavili GPIO pine; uporabljamo logicni nacin, to kar je napisano na GPIO clenu
+GPIO.setmode(GPIO.BCM)
+
+#clk in st sta spremenjlivki, ki povesta kam smo povezali pina (s1, s2)
+#sw je spremenjivka, ki pove kam je povezana tipka(key)
+clk = 17
+dt = 5
+sw = 27
+
+#nastavimo vhodne in izhodne enote
+#GPIO.setup(pin, vhod/izhod, nacin)
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(sw, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#preverimo in nastavimo prvotno stanje
+counter = 0#stevec zacne steti s 0
+clkLastState = GPIO.input(clk)#preberemo stanje na pinu 17
+dtLastState = GPIO.input(dt)#preberemo stanje na pinu 18
+swLastState = GPIO.input(sw)#preberemo stanje na pinu 27 - key
+
+#define functions which will be triggered on pin state changes
+def clkClicked(channel):
+        global counter#uporabimo globalni spremenjivko counter
+        global step#uporabimo globalni spremenjivko step
+
+        clkState = GPIO.input(clk)#preberemo stanje na pinu 17
+        dtState = GPIO.input(dt)#preberemo stanje na pinu 18
+
+        #premikamo se v smeri urinega kazalca, ce je pin 17 == 0 in pin 18 == 1
+        if clkState == 0 and dtState == 1:
+                #pristejemo korak - vrtimo se v smeri urinega kazalca
+                counter = counter + step
+                print ("Counter ", counter)
+
+def dtClicked(channel):
+        global counter
+        global step
+
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        
+        #premikamo se v obratni smeri urinega kazalca, ce je pin 18 == 0 in pin 17 == 1
+        if clkState == 1 and dtState == 0:
+                #odstejemo korak - vrtimo se v obratni smeri urinega kazalca
+                counter = counter - step
+                print("Counter ", counter)
+
+
+def swClicked(channel):
+        global paused#povemo da bomo uporabili gllobalno spremenjivko
+        paused = not paused#obrnemo vrednost spremenjivke poused
+        print ("Paused ", paused)
+
+#na zacetku sprintamo zacetno staje
+print ("Initial clk:", clkLastState)
+print ("Initial dt:", dtLastState)
+print ("Initial sw:", swLastState)
+print ("=========================================")
+
+#nastavimo prekinitve
+#GPIO.add_event_detect(pin, kdaj preberemo FALLING/RISING/ONCHANGE, katero funkcijo naj izvede, nastavimo koliko casa naj prekinitev pocaka pred ponovno izvedbo)
+GPIO.add_event_detect(clk, GPIO.FALLING, callback=clkClicked, bouncetime=50)
+GPIO.add_event_detect(dt, GPIO.FALLING, callback=dtClicked, bouncetime=50)
+GPIO.add_event_detect(sw, GPIO.FALLING, callback=swClicked, bouncetime=50)
+
+input("Start monitoring input")
+
+#pocistimo nastavitve vseh pinov
+GPIO.cleanup()
