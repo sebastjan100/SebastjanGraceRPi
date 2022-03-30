e63f3b26032c8f3b434b90c295c99cf619ef2c96
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-30 12:43:12 +0000

    something

--- uzLed.py
+++ uzLed.py
@@ -15,6 +15,7 @@ pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
+
 def initialize(trig, echo):
         # trig = 26
         # echo = 19
@@ -43,31 +44,6 @@ def distance(trig, echo):
 
         #print("V spremenljivki __name__ se skriva", __name__)
         return razdalja
-
-
-
-
-
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        r = g = b = 0
-    elif pos < 85:
-        r = int(pos * 3)
-        g = int(255 - pos * 3)
-        b = 0
-    elif pos < 170:
-        pos -= 85
-        r = int(255 - pos * 3)
-        g = 0
-        b = int(pos * 3)
-    else:
-        pos -= 170
-        r = 0
-        g = int(pos * 3)
-        b = int(255 - pos * 3)
-    return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
 
 
@@ -113,8 +89,27 @@ if __name__ == "__main__": #izvajamo skripto uzSenzor
                             for j in range(255):
                                 for i in range(num_pixels):
                                     pixel_index = (i * 256 // num_pixels) + j
-                                    pixels[i] = wheel(pixel_index & 255)
-                                pixels.show()
+                                    pos = pixel_index & 255
+                                    pixels[i] = pos
+                            
+                            if pos < 0 or pos > 255:
+                                r = g = b = 0
+                            elif pos < 85:
+                                r = int(pos * 3)
+                                g = int(255 - pos * 3)
+                                b = 0
+                            elif pos < 170:
+                                pos -= 85
+                                r = int(255 - pos * 3)
+                                g = 0
+                                b = int(pos * 3)
+                            else:
+                                pos -= 170
+                                r = 0
+                                g = int(pos * 3)
+                                b = int(255 - pos * 3)
+
+                            pixels.show()
                             
                         time.sleep(0.1)
         except KeyboardInterrupt:
