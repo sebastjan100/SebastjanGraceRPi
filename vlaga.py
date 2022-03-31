4cc25fb34043531157678445def54399497ad645
Sebastjan Tekavc <73191480+sebastjan100@users.noreply.github.com>
2022-03-31 14:23:09 +0000

    a little bit back

--- vlaga.py
+++ vlaga.py
@@ -1,12 +1,9 @@
 import Adafruit_DHT
-import uzSenzor
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
-    razd = uzSenzor.distance()
-    print("Razdalja, ki jo izberemo, znasa %.lf cm. " % razd )
     try:
         if humidity is not None and temperature is not None:
             print("Temperatura je", temperature, "vlaga pa znaša", humidity)
